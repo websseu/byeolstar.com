d470acb4ca54c1a4a471ec5bb91d675db42b4f19
websseu <webstoryboy@naver.com>
2025-06-26 14:03:54 +0900

    2025.06.26

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image.istarbucks.co.kr',
-      },
-    ],
-  },
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
